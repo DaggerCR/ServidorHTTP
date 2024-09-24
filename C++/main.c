663c4e93417f2c16dcfaa9de3f2114b15825ff01
josi marin <marinjosi15@gmail.com>
2024-09-24 16:20:30 -0600

    PUT request

--- C++/main.c
+++ C++/main.c
@@ -30,6 +30,7 @@
     void format_response(char * response_buffer, char *status_code, char *content_type, int content_length, char * file_response);
     void handle_get(int new_socket, char request[]);
     void handle_post(int new_socket,char pRequest[]);
+    void handle_put(int new_socket,char pRequest[]);
 
     int main() 
     {
@@ -126,6 +127,10 @@
             handle_post(new_socket, request);
             printf("\nPOST request manejada\n");
         }
+         else if (strncmp(request, "PUT", 3) == 0) {
+            handle_put(new_socket, request);
+            printf("\nPUT request manejada\n");
+        }
         printf("\nCerrando la conexion del cliente\n");
         close(new_socket);
     }
@@ -141,8 +146,8 @@
     void format_response (char * response_buffer, char *status_code, char *content_type, int content_length, char * file_response) {
 
         sprintf(response_buffer, "HTTP/1.1 %s \r\n "
-                "Content-Type: %s\r\n"
-                " Content-Length: %d\r\n\r\n"
+                "Content-Type: %s \r\n"
+                " Content-Length: %d \r\n\r\n"
                 " %s",
                 status_code, content_type,content_length,file_response);
     }
@@ -190,7 +195,7 @@
 	    const char *file_path =  getFilePath(pRequest);
         	
         // Abrir el archivo response.JSON para escribir el nuevo contenido
-        file = fopen(file_path, "w"); //w para sobre escribir, a para agregar al final
+        file = fopen(file_path, "a"); //w para sobre escribir, a para agregar al final
         if (file == NULL) {
             perror("No se pudo abrir el archivo JSON para escribir");
             exit(EXIT_FAILURE);
@@ -210,9 +215,69 @@
         file = fopen(file_path, "r");
         fread(file_response, sizeof(char), sizeof(file_response), file);
         fclose(file);
+	
+	printf("pRequest is %s", pRequest);
+        // Formar la respuesta HTTP
+        format_response(response_buffer, "200 OK", pRequest, strlen(file_response), file_response);
+
+        // Enviar la respuesta al cliente
+        send(new_socket, response_buffer, strlen(response_buffer), 0);
+        }
+        else
+            printf("\nJSON msj NULO");
+    }
+    
+    // Manejo de PUT request
+    void handle_put(int new_socket, char pRequest[]) {
+        char *file_start;
+        FILE *file;
+        char request[4000] = {0};
+        char response_buffer[2048];
+	char file_response[1024] = {0};
+	char confirmation_message[15];
+	
+        // Encontrar el inicio del JSON en la solicitud POST (después del doble salto de línea)
+        file_start = strstr(pRequest, "\r\n\r\n");
+        file_start += 4;  // Saltar el doble salto de línea
+	    printf("Mensaje por escribir:\r\n%s\r\n", file_start);
+        if (strlen(file_start) > 0)  {
+	    // Agarrar el path del archivo
+	    const char *file_path =  getFilePath(pRequest);
+	    
+        //Ver si el archivo existe, de lo contrario se creara
+        file = fopen(file_path, "r"); 
+        if (file){
+           snprintf(confirmation_message, sizeof(confirmation_message), "200 OK");
+           fclose(file); //luego se abre en modo escritura
+        }
+        else {
+           snprintf(confirmation_message, sizeof(confirmation_message), "201 Created");
+        }
+        
+        // Abrir el archivo response.JSON para escribir el nuevo contenido
+        file = fopen(file_path, "w"); //w para sobre escribir, a para agregar al final
+        if (file == NULL) {
+            printf("No se pudo abrir el archivo JSON para escribir");
+            exit(EXIT_FAILURE);
+        }
+    
+        // Escribir el contenido del JSON en el archivo
+        fwrite(file_start, sizeof(char), strlen(file_start), file);
+		fclose(file);
+	    
+	    //GET CONTENT TYPE
+	    pRequest = strtok(NULL, "\r\n");
+	    pRequest = strtok(NULL, "\r\n");
+	    pRequest = strstr(pRequest, ": ");
+	    pRequest = pRequest+2;
+	    
+        // Responder al cliente (POSTMAN)con un mensaje de éxito
+        file = fopen(file_path, "r");
+        fread(file_response, sizeof(char), sizeof(file_response), file);
+        fclose(file);
 
         // Formar la respuesta HTTP
-        format_response(response_buffer, "201 Created", pRequest, strlen(file_response), file_response);
+        format_response(response_buffer, confirmation_message, pRequest, strlen(file_response), file_response);
 
         // Enviar la respuesta al cliente
         send(new_socket, response_buffer, strlen(response_buffer), 0);
